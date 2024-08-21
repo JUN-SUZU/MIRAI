d1650507faa6296fcb09718979c70a5be5dddc8e
JUN-SUZU <jun2006.master@gmail.com>
2024-08-21 17:20:51 +0900

    change: 年齢の入力から誕生日の入力に変更

--- docs/js/auth.js
+++ docs/js/auth.js
@@ -3,15 +3,22 @@ let miraiKey = localStorage.getItem('miraiKey');
 if (!userID || !miraiKey) {
     window.location.href = '/login/';
 }
+document.getElementById('auth').addEventListener('submit', (event) => {
+    // reCAPTCHAにチェックが入っているか確認
+    if (document.getElementById('g-recaptcha-response').value === '') {
+        event.preventDefault();
+        return;
+    }
+    if (!document.getElementById('birthday').value) {
+        event.preventDefault();
+        return;
+    }
+});
+
 function send() {
     document.getElementById('userID').value = userID;
     document.getElementById('miraiKey').value = miraiKey;
     grecaptcha.enterprise.ready(async () => {
         grecaptcha.enterprise.execute('6Lc-KespAAAAAAXHezZCb2OKM63wu7MxM3Su7IU_', { action: 'auth' });
     });
-    // reCAPTCHAにチェックが入っているか確認
-    if (document.getElementById('g-recaptcha-response').value === '') {
-        alert('reCAPTCHAにチェックを入れてください。');
-        return;
-    }
 }
