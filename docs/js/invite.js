8e7d5e1d2d5476e208a579e2b9aec59b5e2109e8
JUN-SUZU <jun2006.master@gmail.com>
2025-09-20 18:49:58 +0900

    fix: handleInvite関数を定義し、招待処理を修正

--- docs/js/invite.js
+++ docs/js/invite.js
@@ -12,15 +12,17 @@ const closeMessageBox = () => {
 const linkCode = window.location.pathname.split('/')[2];
 const guildId = document.getElementById('guildId').innerText;
 
-const handleInvite = fetch('/invite/api/', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ linkCode: linkCode, guildId: guildId }),
-}).then(response => response.json()).then(data => {
-    if (data.result != 'success') return messageBox(data.message);
-    const inviteLink = data.inviteLink;
-    // 新しいタブで開く
-    window.open(inviteLink, '_blank');
-});
+const handleInvite = () => {
+    fetch('/invite/api/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ linkCode: linkCode, guildId: guildId }),
+    }).then(response => response.json()).then(data => {
+        if (data.result != 'success') return messageBox(data.message);
+        const inviteLink = data.inviteLink;
+        // 新しいタブで開く
+        window.open(inviteLink, '_blank');
+    });
+}
